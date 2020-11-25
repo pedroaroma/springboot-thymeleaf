ce65ce9e6ba7fa7566ccd6a63f9b1031ed7e8112
Pedro Luiz Aroma Filho <pedroaroma@pop-os.localdomain>
2020-11-25 07:33:13 -0300

    Adicionado validacao de acesso ao sistema pelo BD

--- src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package curso.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,6 +15,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 	
 	@Override //configura as solicitações de acesso por http
 	protected void configure(HttpSecurity http) throws Exception {
@@ -33,10 +37,14 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	@Override //cria autenticação do usuário com banco de dados ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-		.withUser("pedro")
-		.password("$2a$10$/vJN53tDsFDnWo9wgLxg5e9s5kNTaBCePI3zcafSro9b1KJ9vmKMm")
-		.roles("ADMIN");
+		auth.userDetailsService(implementacaoUserDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+		
+//		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+//		.withUser("pedro")
+//		.password("$2a$10$/vJN53tDsFDnWo9wgLxg5e9s5kNTaBCePI3zcafSro9b1KJ9vmKMm")
+//		.roles("ADMIN");
 	
 	}
 	
